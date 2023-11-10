7b9ab762f952177480e82a8501fb6f6efd5936d6
GRanger <GRanger@localhost>
2023-11-09 21:20:37 -0600

    11.9.23

--- ContractOne/src/main/java/SimpleSearchHB.java
+++ ContractOne/src/main/java/SimpleSearchHB.java
@@ -11,27 +11,43 @@ import javax.servlet.http.HttpServletResponse;
 import datamodel.Employee;
 import util.Info;
 import util.UtilDB;
+import javafx.application.*;
+
 
 @WebServlet("/SimpleSearchHB")
 public class SimpleSearchHB extends HttpServlet implements Info {
    private static final long serialVersionUID = 1L;
+   
+   
 
    public SimpleSearchHB() {
       super();
+      
+      
    }
 
    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
       String keyword = request.getParameter("keyword").trim();
+      
+      
 
       response.setContentType("text/html");
       PrintWriter out = response.getWriter();
       String title = "Database Result";
       String docType = "<!doctype html public \"-//w3c//dtd html 4.0 transitional//en\">\n"; //
       out.println(docType + //
-            "<html>\n" + //
-            "<head><title>" + title + "</title></head>\n" + //
-            "<body bgcolor=\"#f0f0f0\">\n" + //
-            "<h1 align=\"center\">" + title + "</h1>\n");
+          "<html>\n" + //
+          "<head>");
+      out.println("<title>" + title + "</title>");
+      out.println("<style>");
+      out.println("body {");
+      out.println("  background-image: url('images/bg-banner.png');"); // Specify the path to your background image
+      out.println("  background-repeat: no-repeat;");
+      out.println("  background-size: cover;"); // Adjust background size as needed
+      out.println("}");
+      out.println("</style>");
+      out.println("</head>\n" + //
+          "<h1 align=\"center\">" + title + "</h1>\n");
       out.println("<ul>");
 
       List<Employee> listEmployees = null;
@@ -42,7 +58,11 @@ public class SimpleSearchHB extends HttpServlet implements Info {
       }
       display(listEmployees, out);
       out.println("</ul>");
-      out.println("<a href=/" + projectName + "/" + searchWebName + ">Search Data</a> <br>");
+      out.println("<div style='text-align: left;'><a href='/" + projectName + "/" + searchWebName + "'>Search Data</a></div>" +
+    		    "<div style='text-align: center;'><a href='/" + projectName + "/" + searchWebName + "'>Search Data</a></div>" +
+    		    "<div style='text-align: right;'><a href='/" + projectName + "/" + searchWebName + "'>Search Data</a></div>");
+
+      
       out.println("</body></html>");
    }
 
