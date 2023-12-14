f69314f4dfbb998da040924612c100d26668a3e2
zackt <zackt@10.131.11.90>
2023-12-14 03:47:02 -0600

    Bug four squashed

--- ContractOne/src/main/java/UpdateJob.java
+++ ContractOne/src/main/java/UpdateJob.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,19 +34,28 @@ public class UpdateJob extends HttpServlet implements Info {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String email = (String) session.getAttribute("email");
-		UtilDB.updateJob(Integer.parseInt((String)session.getAttribute("ID")), request.getParameter("status"));
-		if(request.getParameter("status").equals("archive"))
+		if(request.getParameter("status").equals("archive") && !(request.getParameter("ID") == null))
 		{
+			UtilDB.updateJob(Integer.parseInt((String)session.getAttribute("ID")), request.getParameter("status"));
 			UtilDB.updateBid(Integer.parseInt((String)session.getAttribute("ID")));
+		}
+		else if (request.getParameter("ID") == null)
+		{
+			RequestDispatcher rd = request.getRequestDispatcher(CustomerHome);
+			rd.forward(request, response);
+			response.sendRedirect(CustomerHome);
 		}
 		else
 		{
+			UtilDB.updateJob(Integer.parseInt((String)session.getAttribute("ID")), request.getParameter("status"));
 			List<Bid> bids = UtilDB.listBids(Integer.parseInt((String) session.getAttribute("ID")));
 			for (Bid bid : bids)
 			{
 				UtilDB.updateBid(bid.getId(), "Rejected");
 			}
 		}
+		RequestDispatcher rd = request.getRequestDispatcher(CustomerHome);
+		rd.forward(request, response);
 		response.sendRedirect(CustomerHome);
 	}
 
